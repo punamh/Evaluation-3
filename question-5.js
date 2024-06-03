f097ef36459554d58a27395369f5e7683613e7f2
punamh <kumari.punam8649@gmail.com>
2024-06-03 16:00:08 +0530

    answer

---
+++ question-5.js
@@ -0,0 +1,6 @@
+var store = [
+    {name: "Laptop", price: 75000, rating: 4.5},
+    {name: "Tablet", price: 30000, rating: 4.2},
+    {name: "Smartphone", price: 60000, rating: 4.7},
+    {name: "Smartwatch", price: 20000, rating: 3.9}
+];
